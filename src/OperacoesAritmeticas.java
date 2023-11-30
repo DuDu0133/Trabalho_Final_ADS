f453963cf47ac6e179b4c38e03d57ceba47eb606
sniperdogshot <125098573+sniperdogshot@users.noreply.github.com>
2023-11-30 14:44:14 -0300

    Update OperacoesAritmeticas.java

--- src/OperacoesAritmeticas.java
+++ src/OperacoesAritmeticas.java
@@ -8,6 +8,14 @@ public class OperacoesAritmeticas {
         // Exemplo de subtração
         int resultadoSubtracao = subtrair(10, 4);
         System.out.println("Subtração: " + resultadoSubtracao);
+
+        // Exemplo de multiplicação
+        int resultadoMultiplicacao = multiplicar(6, 7);
+        System.out.println("Multiplicação: " + resultadoMultiplicacao);
+
+        // Exemplo de divisão
+        double resultadoDivisao = dividir(15, 3);
+        System.out.println("Divisão: " + resultadoDivisao);
     }
 
     // Função para realizar a soma
@@ -18,5 +26,19 @@ public class OperacoesAritmeticas {
     // Função para realizar a subtração
     public static int subtrair(int a, int b) {
         return a - b;
+    }
+    // Função para realizar a multiplicação
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    // Função para realizar a divisão
+    public static double dividir(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            System.out.println("Erro: Divisão por zero não permitida.");
+            return Double.NaN;
+        }
     }
 }
